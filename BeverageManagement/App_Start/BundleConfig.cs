66623e7127016df974e2fa0f484d89eaf3ca170f
Rashik Hasnat <rashikhasnat@ymail.com>
2016-07-25 15:19:36 +0600

    jquery 2 updated, bundle config modified to get more minified files.
    jquery 2 updated, bundle config modified to get more minified files.

--- BeverageManagement/App_Start/BundleConfig.cs
+++ BeverageManagement/App_Start/BundleConfig.cs
@@ -7,11 +7,9 @@ namespace BeverageManagement {
 
             var cssPath = "~/Content/css/";
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery-ui.min.js"));
+                        "~/Scripts/jquery/jquery-2.2.4.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -19,39 +17,48 @@ namespace BeverageManagement {
                         "~/Scripts/modernizr-*"));
 
 #if DEBUG
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/bootstrap-table.js",
-                      "~/Scripts/bootstrap-table-filter.js",
-                      "~/Scripts/bootstrap-table-export.js",
-                      "~/Content/TinyMce/tinymce.min.js",
-                      "~/Scripts/moment.js",
-                      "~/Scripts/bootstrap-datetimepicker.min.js",
-                      "~/Scripts/Custom.js",
-                      "~/Scripts/respond.js"));
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-              cssPath + "bootstrap.min.css",
-              cssPath + "bootstrap-table.css",
-              cssPath + "font-awesome.css",
-              cssPath + "custom-style.css",
-              cssPath + "site.css",
-              cssPath + "bootstrap-datetimepicker.min.css",
-              cssPath + "bootstrap-datetimepicker-override.css"
-              ));
-            BundleTable.EnableOptimizations = false;
-            bundles.UseCdn = false;
-#else
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Content/bundles/javascripts.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                    "~/Scripts/jquery.validate.min.js",
+                    "~/Scripts/jquery.validate.unobtrusive.min"
+             ));
 
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                    "~/Scripts/bootstrap.js",
+                    "~/Scripts/bootstrap-table.js",
+                    "~/Scripts/bootstrap-table-filter.js",
+                    "~/Scripts/bootstrap-table-export.js",
+                    "~/Content/TinyMce/tinymce.min.js",
+                    "~/Scripts/moment.js",
+                    "~/Scripts/bootstrap-datetimepicker.min.js",
+                    "~/Scripts/Custom.js",
+                    "~/Scripts/respond.js"));
             bundles.Add(new StyleBundle("~/Content/css").Include(
-              "~/Content/bundles/styles.min.css"
+                    cssPath + "bootstrap.min.css",
+                    cssPath + "bootstrap-table.css",
+                    cssPath + "font-awesome.css",
+                    cssPath + "custom-style.css",
+                    cssPath + "site.css",
+                    cssPath + "bootstrap-datetimepicker.min.css",
+                    cssPath + "bootstrap-datetimepicker-override.css"
+              ));
+
+#else
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                    "~/Content/bundles/jquery-validations.min.js"
             ));
 
-            BundleTable.EnableOptimizations = true;
-            bundles.UseCdn = true;
-#endif
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                     "~/Content/bundles/javascripts.min.js"
+            ));
 
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                    "~/Content/bundles/styles.min.css"
+            ));
+
+
+#endif
+            BundleTable.EnableOptimizations = false;
+            //bundles.UseCdn = true;
         }
     }
 }
